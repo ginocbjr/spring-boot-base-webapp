c2abd08f3aa36b22ba176b823cf942b3dac903e3
sonnyjustine <sonnyjustine06@gmail.com>
2015-09-05 17:57:48 +0800

    Added static resources and html pages

--- src/main/java/org/networking/web/controller/HomeController.java
+++ src/main/java/org/networking/web/controller/HomeController.java
@@ -13,9 +13,54 @@ public class HomeController {
     String index() {
         return "index";
     }
-
+    
     @RequestMapping(value = {"/login", "/logout"})
     String login() {
         return "login";
+    }
+    
+    @RequestMapping("/contact")
+    String homeContact() {
+        return "contact";
+    }
+    
+    @RequestMapping("/infinite-business-solutions")
+    String homeIbs() {
+        return "ibs";
+    }
+    
+    @RequestMapping("/products")
+    String product() {
+        return "product";
+    }
+    
+    @RequestMapping("/product-1")
+    String product1() {
+        return "product-1";
+    }
+    
+    @RequestMapping("/product-2")
+    String product2() {
+        return "product-2";
+    }
+    
+    @RequestMapping("/product-3")
+    String product3() {
+        return "product-3";
+    }
+    
+    @RequestMapping("/members")
+    String members() {
+        return "member-list";
+    }
+    
+    @RequestMapping("/order")
+    String order() {
+        return "admin-order";
+    }
+    
+    @RequestMapping("/admin-products")
+    String adminProducts() {
+        return "admin-product-list";
     }
 }
